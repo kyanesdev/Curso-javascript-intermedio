b2e5be084929eb7510ba166a8a58cf899cdb370f
kyanesdev <kyanesdev@gmail.com>
2021-10-17 17:36:24 -0300

    seguimos con promesas

--- Capitulo-12.js
+++ Capitulo-12.js
@@ -3,7 +3,7 @@
 //Callback: En resumen es una funcion que le pasamos como parametro otra funcion
 
 //1era Forma
-function prueba(callback){ //aca tenemos la funcion dentro de la funcion
+/*function prueba(callback){ //aca tenemos la funcion dentro de la funcion
     callback("kevin"); //aca ejecutamos esa funcion "callback"
 }
 
@@ -19,3 +19,39 @@ prueba(function decirN(nom){
     console.log(nom);
 })
 
+console.log("---------------------------------------------");
+console.log("---------------------------------------------");
+
+//Promesas
+//Son un objeto con 2 callbacks "reset" "resolve" , que representan algo
+
+
+//Los datos se encuentran encapsulados
+*/
+
+/*
+let nombre = "pedro"; 
+
+const promesa = new Promise((resolve,reject)=>{
+    if(nombre != "pedro"){
+        reject("lo siento, el nombre no es pedro");
+    }else{
+        resolve(nombre);
+    }
+
+});
+
+console.log(promesa);
+
+//Para acceder a los datos encapsulados:
+
+//Esto solo se ejecuta en el resolve (porque no hubo ningun problema)
+//Pero los errores los podes manejar con un catch
+promesa.then((resultado)=>{
+    console.log(resultado);
+}).catch((e)=>{
+    console.log(e);
+})
+*/
+
+//Await & Async.
