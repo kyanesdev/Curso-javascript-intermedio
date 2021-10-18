5b63e578f6bf5a9d47f9cee141eb954489bba0bb
kyanesdev <kyanesdev@gmail.com>
2021-10-18 01:15:50 -0300

    finalizado ejercicio cofla cap12

--- Capitulo-12.js
+++ Capitulo-12.js
@@ -55,3 +55,83 @@ promesa.then((resultado)=>{
 */
 
 //Await & Async.
+/* 
+const objeto = {
+    propiedad1 : "valor1",
+    propiedad2 : "valor2",
+    propiedad3 : "valor3"
+};
+
+
+const obtenerInformacion = ()=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=> {resolve(objeto)},3000);
+    })
+    
+    
+}
+*/
+//obtenerInformacion().then(resultado => console.log(resultado));
+/*
+const mostrarResultado = async()=>{
+    resultado = await obtenerInformacion();
+    console.log(resultado);
+}
+
+mostrarResultado();
+*/ 
+
+//Cofla
+
+const materiasHTML = document.querySelector(".materias");
+const materias = [
+    {
+        nombre: "Fisica 2",
+        nota: 4
+    },
+    {
+        nombre: "Ingenieria Web 2",
+        nota: 8
+    } ,
+    {
+        nombre: "Bases de Datos 2",
+        nota: 6
+    },
+    {
+        nombre: "Programación Funcional",
+        nota: 5
+    }
+]
+
+
+const obtenerMateria = (id)=>{
+    return new Promise((resolve,reject)=>{
+        materia = materias[id];
+        if(materia == undefined){
+            reject("No existe la materia");
+        }else{
+            setTimeout(()=>{resolve(materia)},Math.random()*500);
+        }
+    })
+}
+
+//obtenerMateria(0).then(res=>console.log(res));
+
+const devolverMaterias = async()=>{
+    let materia =[];
+
+    for (var i = 0; i < materias.length; i++) {
+        materia[i] = await obtenerMateria(i);
+        console.log(materia[i]);
+        let newHTMLCode=`
+        <div class="materia">
+            <div class="nombre">${materia[i].nombre}</div>
+            <div class="nota">${materia[i].nota}</div>
+        </div>
+        `;
+        materiasHTML.innerHTML += newHTMLCode;
+
+    }
+}
+
+devolverMaterias();
