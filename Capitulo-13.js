012517c441846e62a0a4e2deb7117331896db61e
kyanesdev <kyanesdev@gmail.com>
2021-10-21 01:51:36 -0300

    cap 13 y uso de sass

---
+++ Capitulo-13.js
@@ -0,0 +1,44 @@
+
+//Objeto tipo JSON (Deserializado)
+// const objeto = {
+
+//     "variable1":"Kevin",
+//     "variable2":"Yanes"
+
+// };
+
+// console.log(objeto);
+
+//Se necesita serializar para enviar los datos en la red
+//Cuando lo enviamos, debemos mandarlo serializado...De la misma forma , lo recibimos serializado
+//cuando lo pedimos.
+
+// const objeto1 = {"variable3":"Ladri","variable4":"Disandro"};
+
+// console.log(typeof objeto1);
+
+//------------------------------------------------------------------
+
+//De esta forma se serializa
+
+// var objeto = {
+
+//     "variable1":"Kevin",
+//     "variable2":"Yanes"
+
+// };
+// console.log(typeof objeto);
+// objeto = JSON.stringify(objeto);
+// console.log(typeof objeto);
+
+//------------------------------------------------------------------
+
+//Deserializar un objeto serializado
+
+// var objeto = '{"variable1":"Kevin","variable2":"Yanes"}';
+// console.log(typeof objeto);
+// objeto = JSON.parse(objeto);
+// console.log(typeof objeto);
+
+//------------------------------------------------------------------
+
